b4e33741df09750756a9432117c74fead809db7a
nandinicbit1981 <nandinicbit@gmail.com>
2017-06-03 23:10:08 -0700

    updating library

---
+++ jokeslibrary/src/main/java/com/example/JokeClass.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class JokeClass {
+    public static void main(String[] args) {
+        System.out.println("Hello There");
+    }
+
+    public static String getJoke() {
+        return "This is a joke";
+    }
+}
